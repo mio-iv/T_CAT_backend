76d9ef92cf296488129fa87d1ac7281fa24d7270
mio-iv <mioiv.nano@gmail.com>
2023-12-17 16:06:43 +0900

    view作成

--- T_CAT/views.py
+++ T_CAT/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from drf_spectacular.utils import extend_schema
+from rest_framework import status, views
+from rest_framework.response import Response
 
-# Create your views here.
+from T_CAT.serializers import OpenAIAPIBatchUpdateSerializer
+
+@extend_schema(
+    tags=["OpenAIAPI"],
+    request=OpenAIAPIBatchUpdateSerializer,
+    responses=OpenAIAPIBatchUpdateSerializer,
+    )
+
+class OpenAIAPIBatchUpdateView(views.APIView):
+    "openAIへのリクエストを受け付ける"
+
+    def post(self, request, *args, **kwargs):
+        serializer = OpenAIAPIBatchUpdateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_200_OK)
