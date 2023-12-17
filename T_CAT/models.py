9054e2ed7c04db40b4b5bb9f617cdda8923d646e
mio-iv <mioiv.nano@gmail.com>
2023-12-17 16:04:16 +0900

    テーブル定義追加

--- T_CAT/models.py
+++ T_CAT/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class OoenAIAPI(models.Model):
+    "openAIへのリクエスト"
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    input_message = models.CharField(max_length=500, blank=True)
+    add_message = models.CharField(max_length=200, blank=True)
+    output_message = models.CharField(max_length=500, null=True, blank=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "openai_api"
+        verbose_name = verbose_name_plural = "openAIへのリクエスト"
+        ordering = ["create_at"]
+
+    def __str__(self):
+        return str(self.id)
