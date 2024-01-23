ec0fb0f6f98fae5aa4b9a159cb7fe4611685ce67
mio-iv <mioiv.nano@gmail.com>
2024-01-23 20:43:49 +0900

    リクエスト例を変更

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 OPENAIへのリクエストを受け付けます。
 
 依頼例：
-> input_message: 明日の天気は？
-> add_message: 小学生むけに答えて
+> input_message: 空は何で青いの？
+> add_message: 幼児向けに易しく答えて
 
 > input_message: いい加減にしてよ、めっちゃ迷惑！
 > add_message:この文をオブラートに包んだ表現に変えて
--- T_CAT/utils.py
+++ T_CAT/utils.py
@@ -1,10 +1,13 @@
+import T_CAT.consts as consts
+
 def create_message(input_message, add_message):
     """
     input_message, add_messageを、一つの文にする。
-    input_message: 明日の天気は？
-    add_message: 小学生むけに答えて
+    例：
+    input_message: 空は何で青いの？
+    add_message: 幼児向けに易しく答えて
 
     input_message: いい加減にしてよ、めっちゃ迷惑！
     add_message:この文をオブラートに包んだ表現に変えて
     """
-    return add_message + "「" + input_message + "」"
+    return  f"{str(consts.MAX_TOKENS)}文字以内で" + add_message + "「" + input_message + "」"
